84abb7cbe5f339a37aa1cf07c1c28ab31b7cc450
mohamad malek <mohamadmalek@gmail.com>
2025-08-08 06:57:52 +0300

    make tha basic crud operations and their validation requests

---
+++ app/Http/Controllers/TaskController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreTaskRequest;
+use App\Http\Requests\UpdateTaskRequest;
+use App\Models\Task;
+use Illuminate\Http\Request;
+
+class TaskController extends Controller
+{
+    
+    
+    public function store(StoreTaskRequest $request)
+    {
+        $task=Task::create($request->validated());
+
+        return response()->json($task,201);
+    }
+
+
+
+    public function update(UpdateTaskRequest $request,$id)
+    {   
+        $task = Task::findOrFail($id);
+        $task->update($request->validated());
+        return  response()->json($task,200);
+    }
+
+    public function destroy($id)
+    {
+        $task =Task::findOrFail($id);
+        $task->delete();
+        return response()->json(null,204);
+    }
+
+
+    public function index()
+    {
+        $task = Task::all();
+        return response()->json($task,200);
+    }
+
+    public function show($id)
+    {
+        $task =Task::findOrFail($id);
+        return response()->json($task,200);
+    }
+
+        
+
+}
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\TaskController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -8,4 +9,4 @@ Route::get('/user', function (Request $request) {
 })->middleware('auth:sanctum');
 
 
-
+Route::apiResource('/tasks',TaskController::class);
